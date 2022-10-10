34ae0e20937f4e603d1bb41ccc158d37ed68f6c8
hcoullon <helene.coullon@inria.fr>
2022-10-10 12:02:25 +0200

    update port

--- movie/movie.py
+++ movie/movie.py
@@ -5,7 +5,7 @@ from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 
-PORT = 3200
+PORT = 5001
 HOST = '0.0.0.0'
 
 with open('{}/databases/movies.json'.format("."), "r") as jsf:
@@ -15,6 +15,13 @@ with open('{}/databases/movies.json'.format("."), "r") as jsf:
 @app.route("/", methods=['GET'])
 def home():
     return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+
+# get the complete json file
+@app.route("/json", methods=['GET'])
+def get_json():
+    #res = make_response(jsonify(INFO), 200)
+    res = make_response(jsonify(movies), 200)
+    return res
 
 if __name__ == "__main__":
     #p = sys.argv[1]
